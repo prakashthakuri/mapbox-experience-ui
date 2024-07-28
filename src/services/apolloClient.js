f5c8a28a8555d2b02ce1bffea8be81267aca9270
Prakash Thakuri <prakashthakuri1994@gmail.com>
2024-07-28 01:34:21 -0500

    Update apolloClient.js

--- src/services/apolloClient.js
+++ src/services/apolloClient.js
@@ -1,10 +1,18 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
     uri: process.env.VITE_REACT_APP_GRAPHQL_URI ||  'http://localhost:8080/graphql',
 })
-
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            "x-apollo-operation-name": "mapbox-service", 
+            "apollo-require-preflight": "true" 
+        }
+    };
+});
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: httpLink
-})
+    link: authLink.concat(httpLink),})
